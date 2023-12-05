a6924edf0d3c5a26b287b47fa782295c97c1463b
Belial <lautarogazzo1@gmail.com>
2023-12-04 23:02:07 -0300

    GitIgnore

--- .gitignore/ProyectoFinal-2/AppCoder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.http import HttpResponse
-from AppCoder.views import *
-
-urlpatterns = [
-    path("cursos/", cursos_view),
-    path("inicio", inicio_view)
-]
